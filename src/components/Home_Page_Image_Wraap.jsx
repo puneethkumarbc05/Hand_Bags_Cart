cf753d0da85b81840c569b02e907f5362ba0363f
puneeth_kumar_b_c <puneeth.kumar@c2info.com>
2024-02-12 10:16:49 +0530

    react code added

---
+++ src/components/Home_Page_Image_Wraap.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home_Page_Image_Wraap = () => {
+    return (
+        <div className='home_page_image_wraap'>
+            <div>CRODD BODY</div>
+            <div>GET UP TO</div>
+            <div>50% OFF</div>
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui tenetur at pariatur veniam fugit, molestiae cumque? Itaque enim consectetur quam veniam facere impedit repellendus similique labore, officia rerum praesentium excepturi.</div>
+            <div>SHOP NOW!</div>
+        </div>
+    )
+}
+
+export default Home_Page_Image_Wraap
